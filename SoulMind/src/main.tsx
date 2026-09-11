eb6f06d26529310488f8d2cb2c3a0ce8b0200081
Ludmylla1905 <ludmilanogueira231@gmail.com>
2026-09-11 02:01:24 -0300

    feat: adiciona navegação entre missões e detalhes

--- SoulMind/src/main.tsx
+++ SoulMind/src/main.tsx
@@ -11,6 +11,7 @@ import Error from './pages/Error/index.tsx';
 import Faq from './pages/Faq/index.tsx';
 import Home from './pages/Home/index.tsx';
 import Missoes from './pages/Missoes/index.tsx';
+import DetalhesMissao from './pages/DetalhesMissao/index.tsx';
 import Personalizar from './pages/Personalizar/index.tsx';
 import Progresso from './pages/Progresso/index.tsx';
 import QuemSomos from './pages/QuemSomos/index.tsx';
@@ -26,6 +27,7 @@ const router = createBrowserRouter([
       { path: "/contato", element: <Contato /> },
       { path: "/faq", element: <Faq /> },
       { path: "/missoes", element: <Missoes /> },
+      { path: "/detalhes-missao/:id", element: <DetalhesMissao /> },
       { path: "/personalizar", element: <Personalizar /> },
       { path: "/progresso", element: <Progresso /> },
       { path: "/quem-somos", element: <QuemSomos /> },
