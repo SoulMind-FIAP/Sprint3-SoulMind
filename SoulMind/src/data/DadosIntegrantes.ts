5a283ba95ec175fa2e165e221f5c9f0571560109
Pedronuf <pp23104554@gmail.com>
2026-09-12 11:41:53 -0300

    feat: criação de tipos e dados dos integrantes

---
+++ SoulMind/src/data/DadosIntegrantes.ts
@@ -0,0 +1,41 @@
+import type { TipoIntegrantes } from "../types/TipoIntegrantes";
+import Melissa from "../assets/melissa.jpeg";
+import Ludmylla from "../assets/lud.jpeg";
+import Henrique from "../assets/henrique.jpeg";
+import Pedro from "../assets/pedro.png";
+export const DadosIntegrantes: TipoIntegrantes[] = [
+    {
+        id: 1,
+        nome: "Melissa Fiuza",
+        rm: "RM 573695",
+        turma: "1TDSPO",
+        foto: Melissa,
+        github: "https://github.com/melissafiuza",
+        linkedin: "https://linkedin.com/in/melissa-fiuza/"
+    },
+    {
+        id: 2,
+        nome: "Ludmylla Lins Nogueira",
+        rm: "RM 572999",
+        turma: "1TDS...",
+        foto: Ludmylla,
+        github: "https://github.com/Ludmylla1905",
+        linkedin: "https://linkedin.com/in/ludmylla-nogueira-11991927a/"
+    },
+
+    {id: 3,
+        nome: "Henrique Tavares",
+        rm: "RM 569336",
+        turma: "1TDS...",
+        foto: Henrique,
+        github: "https://github.com/eneiqmaneiro",
+        linkedin: "https://linkedin.com/in/henrique-tavares-coutinho-81a512356/"},
+
+        {id: 4,
+        nome: "Pedro Nunes",
+        rm: "RM 573066",
+        turma: "1TDS...",
+        foto: Pedro,
+        github: "https://github.com/PedroNuf",
+        linkedin: "https://linkedin.com/in/pedro-nunes-1089133b4/"}
+];
---
+++ SoulMind/src/types/TipoIntegrantes.ts
@@ -0,0 +1,9 @@
+export type TipoIntegrantes = {
+    id: number;
+    nome: string;
+    rm: string;
+    turma: string;
+    foto: string;
+    github: string;
+    linkedin: string;
+}
